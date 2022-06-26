b014186500493496ba8e984993cf742d9b1ef732
Meiri Finish <meiri41121@gmail.com>
2022-06-26 17:42:05 +0300

    add swager

--- complete/src/main/java/com/chess/ChessController.java
+++ complete/src/main/java/com/chess/ChessController.java
@@ -5,8 +5,8 @@ import com.chess.service.ChessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,24 +15,24 @@ public class ChessController {
 	@Autowired
 	private ChessService cs;
 
-	@GetMapping("/")
+	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String newGame(Model model) {
 		return "newGame";
 	}
 
-	@PostMapping("/newGame")
+	@RequestMapping(method = RequestMethod.POST, value = "/newGame")
 	public String newGamePost(@RequestParam(name="name") String name, @RequestParam(name="difficulty") int difficulty, Model model) {
 		cs.StartGame(model, name, difficulty);
 		return "chess";
 	}
 
-	@PostMapping("/chess")
+	@RequestMapping(method = RequestMethod.POST, value = "/chess")
 	public String chessMove(@RequestParam(name="move") String move, @RequestParam(name="gameId") long gameId, Model model) {
 		System.out.println(move);
 		return cs.move(model, gameId, move);
 	}
 
-	@PostMapping("/newPawn")
+	@RequestMapping(method = RequestMethod.POST, value = "/newPawn")
 	public String pawnChange(@RequestParam(name="newPawn") String newPawn, @RequestParam(name="gameId") long gameId, Model model) {
 		System.out.println(newPawn);
 		return cs.newPawn(model, gameId, newPawn);
